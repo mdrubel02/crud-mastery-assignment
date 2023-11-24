79dbf5ff9b1f15cf142c4650b3fd900da6c0fd42
Md Ibrahim <mdrubel072125@gmail.com>
2023-11-23 20:56:40 -0800

    'user-interface-create'

---
+++ src/app/modules/users/user.interface.ts
@@ -0,0 +1,31 @@
+
+
+export type UserFullName =  {
+    firstName: string;
+    lastNmae:string
+}
+
+export type UserAddress = {
+    street: string;
+    city: string;
+    country: string
+}
+
+export type UserOders = {
+    producNmae:string;
+    price: string;
+    quantity:number
+}[]
+
+export type User = {
+    userId : number;
+    userName: string;
+    password: string;
+    fullName:UserFullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address:UserAddress;
+    orders: UserOders
+}
---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,8 @@
+// import {Schema} from 'mongoose'
+// import { User } from './user.interface'
+
+//  const userSchema = new Schema<User>({
+
+// }) 
+
+// export const User = model 
