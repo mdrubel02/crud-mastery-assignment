90269aa6bc1be763e11c387ecd0cab989b9a101b
Md Ibrahim <mdrubel072125@gmail.com>
2023-11-24 00:49:11 -0800

    update-and-single-get-user

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -19,6 +19,49 @@ const createUser = async (req : Request, res : Response)=>{
     }
 }
 
+const getUser =async (req: Request, res : Response ) => {
+    try {
+        const result = await UserServices.getAllUserIntoDB()
+        res.status(200).json({
+            success: true,
+            message: "User fetched successfully!",
+            data: result
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+const getSingleUser = async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id
+      const result = await UserServices.getSingleUserIntoDB(id)
+      res.status(200).json({
+        status: 'success',
+        message: 'Single user fetched successfully',
+        data: result,
+      })
+    } catch (error ) {
+      console.log(error)
+    }
+  }
+const updateUser = async (req: Request, res: Response) => {
+    try {
+      const tourData = req.body
+      const id = req.params.id
+      const result = await UserServices.updateUser(id, tourData)
+      res.status(200).json({
+        status: 'success',
+        message: 'User updated successfully',
+        data: result,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 export const UserController = {
-    createUser
+    createUser,
+    getUser,
+    getSingleUser,
+    updateUser
 }
--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -8,6 +8,29 @@ const createUserIntoDB = async (user : User)=>{
     return result
  }
 
+const getAllUserIntoDB =async () => {
+    const result = await UserModel.find({})
+    return result;
+}
+const getSingleUserIntoDB =async (id:string) => {
+    const result = await UserModel.findById(id);
+    return result
+}
+const updateUser =async (id: string, userData: User):Promise<User | null> => {
+    const result = await UserModel.findByIdAndUpdate(id, userData, {
+        new: true,
+        runValidators: true,
+      })
+      return result
+}
+const deleteUser = async (id: string): Promise<User | null> => {
+    const result = await UserModel.findByIdAndDelete(id)
+    return result
+  }
  export const UserServices = {
-    createUserIntoDB
+    createUserIntoDB,
+    getAllUserIntoDB,
+    getSingleUserIntoDB,
+    updateUser,
+    deleteUser
 }
